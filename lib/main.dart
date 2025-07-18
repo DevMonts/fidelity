fc35317cddc12fe4b9dff2295d0c36561e2e6eaf
Davi Monteiro <montsgit@gmail.com>
2025-07-17 21:15:40 -0300

    build: sqflite to desktop

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:fidelity_app/app.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 //import 'package:hive_flutter/adapters.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //await Hive.initFlutter();
   //await Hive.openBox('users');
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
   runApp(const App());
 }
