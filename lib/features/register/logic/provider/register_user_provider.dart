e560023c66089e71517991b52d02c69fd9c099f9
Davi Monteiro <montsgit@gmail.com>
2025-07-21 15:07:48 -0300

    cleanup: user provider

--- lib/features/register/logic/provider/register_user_provider.dart
+++ lib/features/register/logic/provider/register_user_provider.dart
@@ -1,8 +1,6 @@
-//mport 'package:fidelity_app/common/constants/app_strings.dart';
 import 'package:fidelity_app/common/constants/app_strings.dart';
 import 'package:fidelity_app/common/helpers/db_helper.dart';
 import 'package:flutter/material.dart';
-//import 'package:hive/hive.dart';
 
 class RegisterUserProvider extends ChangeNotifier {
   bool isLoading = false;
@@ -11,7 +9,6 @@ class RegisterUserProvider extends ChangeNotifier {
     required String userEmail,
     required String userPassword,
   }) async {
-    // final box = await Hive.openBox('users');
     isLoading = true;
     notifyListeners();
     final dbHelper = DbHelper.instance;
@@ -19,21 +16,13 @@ class RegisterUserProvider extends ChangeNotifier {
       userEmail,
       userPassword,
     );
-    if (
-    //box.containsKey(
-    emailExists
-    //)
-    ) {
-      //   return
+    if (emailExists) {
       message = AppStrings.alreadyExists;
     } else {
-      await
-      //box.put(email, password);
-      dbHelper.createUser({
+      await dbHelper.createUser({
         'userEmail': userEmail,
         'userPassword': userPassword,
       });
-      //   return
       message = AppStrings.success;
     }
     isLoading = false;
