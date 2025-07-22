b88de17ac4ee69550edb97cfbcc03c7718083977
Davi Monteiro <montsgit@gmail.com>
2025-07-22 17:37:16 -0300

    style: vinculate page

--- lib/features/vinculation/presentation/page/vinculate_page.dart
+++ lib/features/vinculation/presentation/page/vinculate_page.dart
@@ -25,86 +25,93 @@ class _VinculatePageState extends State<VinculatePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: AppStrings.userIdLabel,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: AppStrings.userIdLabel,
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: userIdController,
+                    keyboardType: TextInputType.number,
                   ),
-                  controller: userIdController,
-                  keyboardType: TextInputType.number,
                 ),
-              ),
-              Expanded(
-                child: TextButton(
-                  onPressed: () async {
-                    final provider = Provider.of<VinculateProvider>(
-                      context,
-                      listen: false,
-                    );
-                    final mesage = await provider.vinculate(
-                      userId: int.parse(userIdController.text.trim()),
-                      enterpriseId: int.parse(
-                        enterpriseIdController.text.trim(),
-                      ),
-                    );
-                    ScaffoldMessenger.of(
-                      context,
-                    ).showSnackBar(SnackBar(content: Text(mesage)));
-                    setState(() {
-                      vinculation = DbHelper.instance.vinculationHelper
-                          .showVinculations();
-                    });
-                  },
-                  child: Text(AppStrings.vinculate),
-                ),
-              ),
-              Expanded(
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    labelText: AppStrings.enterpriseIdLabel,
-                  ),
-                  controller: enterpriseIdController,
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-            ],
-          ),
-          Expanded(
-            child: FutureBuilder<List<VinculationModel>>(
-              future: vinculation,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.hasError) {
-                  return Text(snapshot.error.toString());
-                }
-                if (snapshot.hasData && snapshot.data!.isNotEmpty) {
-                  return ListView.builder(
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index) {
-                      final vinculation = snapshot.data![index];
-                      return Card(
-                        child: Text(
-                          '${vinculation.userId} ${vinculation.enterpriseId}',
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      final provider = Provider.of<VinculateProvider>(
+                        context,
+                        listen: false,
+                      );
+                      final mesage = await provider.vinculate(
+                        userId: int.parse(userIdController.text.trim()),
+                        enterpriseId: int.parse(
+                          enterpriseIdController.text.trim(),
                         ),
                       );
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(SnackBar(content: Text(mesage)));
+                      setState(() {
+                        vinculation = DbHelper.instance.vinculationHelper
+                            .showVinculations();
+                      });
                     },
-                  );
-                }
-                if (snapshot.hasData && snapshot.data!.isEmpty) {
-                  return Center(child: Text(AppStrings.empty));
-                } else {
-                  return const Text(AppStrings.generalError);
-                }
-              },
+                    child: Icon(Icons.swap_horiz),
+                    style: ElevatedButton.styleFrom(shape: CircleBorder()),
+                  ),
+                ),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: AppStrings.enterpriseIdLabel,
+                      border: OutlineInputBorder(),
+                    ),
+                    controller: enterpriseIdController,
+                    keyboardType: TextInputType.number,
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+            SizedBox(height: 10),
+            Expanded(
+              child: FutureBuilder<List<VinculationModel>>(
+                future: vinculation,
+                builder: (context, snapshot) {
+                  // if (snapshot.connectionState == ConnectionState.waiting) {
+                  //   return Center(child: CircularProgressIndicator());
+                  // }
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  }
+                  if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                    return Table(
+                      border: TableBorder.all(),
+                      children: snapshot.data!.map((vinculation) {
+                        return TableRow(
+                          children: [
+                            Center(child: Text('${vinculation.userId}')),
+                            Center(child: Text('${vinculation.enterpriseId}')),
+                          ],
+                        );
+                      }).toList(),
+                    );
+                  }
+                  if (snapshot.hasData && snapshot.data!.isEmpty) {
+                    return Center(child: Text(AppStrings.empty));
+                  } else {
+                    return const Text(AppStrings.generalError);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
